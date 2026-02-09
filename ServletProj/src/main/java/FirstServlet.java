2c159995de5ef005cf079d1f711685dcb1c035a6
kisshor-1113 <kisshorm1310@gmail.com>
2026-02-09 19:07:47 +0530

    Add files via upload

---
+++ ServletProj/src/main/java/FirstServlet.java
@@ -0,0 +1,74 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public FirstServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request,response);
+		
+			response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+
+		/**
+		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+		 */
+	
+			try {
+				response.setContentType("text/html");
+				PrintWriter out=response.getWriter();
+				
+				String n=request.getParameter("user");
+				out.println("Hello"+n);
+				
+				// Creating Cookies
+				Cookie ck=new Cookie("user",n);
+				response.addCookie(ck);
+				// creating submit button
+				out.print("<form action='SecondServlet'>");
+				out.print("<input type='submit' value='go' ");
+				out.print("</form>");
+				
+			}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+		}
+
+	
+	}
+	
+	
+
+
