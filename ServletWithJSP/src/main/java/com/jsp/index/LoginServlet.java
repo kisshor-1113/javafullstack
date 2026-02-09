e559ad73dd030070336221f2e29157820cbffa67
kisshor-1113 <kisshorm1310@gmail.com>
2026-02-09 19:10:26 +0530

    Add files via upload

---
+++ ServletWithJSP/src/main/java/com/jsp/index/LoginServlet.java
@@ -0,0 +1,44 @@
+package com.jsp.index;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+//@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 String name = request.getParameter("uname");
+		 
+		 //set data to request
+		 request.setAttribute("username", name);
+		 
+		 //forward to jsp
+		 RequestDispatcher rd = request.getRequestDispatcher("welcome.jsp");
+		 rd.forward(request, response);
+	}
+
+}
