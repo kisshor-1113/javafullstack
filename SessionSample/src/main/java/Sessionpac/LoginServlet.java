88ca99f521e2fe42836836b10cc0a4cb06e48382
kisshor-1113 <kisshorm1310@gmail.com>
2026-02-09 19:12:23 +0530

    Add files via upload

---
+++ SessionSample/src/main/java/Sessionpac/LoginServlet.java
@@ -0,0 +1,43 @@
+package Sessionpac;
+
+	import jakarta.servlet.ServletException;
+	import jakarta.servlet.annotation.WebServlet;
+	import jakarta.servlet.http.HttpServlet;
+	import jakarta.servlet.http.HttpServletRequest;
+	import jakarta.servlet.http.HttpServletResponse;
+	import jakarta.servlet.http.HttpSession;
+
+	import java.io.IOException;
+
+
+	@WebServlet("/LoginServlet")
+	public class LoginServlet extends HttpServlet {
+		private static final long serialVersionUID = 1L;
+	       
+	    /**
+	     * @see HttpServlet#HttpServlet()
+	     */
+	    public LoginServlet() {
+	        super();
+	        // TODO Auto-generated constructor stub
+	    }
+
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			response.getWriter().append("Served at: ").append(request.getContextPath());
+		}
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			String username = request.getParameter("username");
+			// create and store the session
+			HttpSession session = request.getSession();
+			session.setAttribute(username, username);
+			
+			//redirect to another servlet
+			response.sendRedirect("WelcomeServlet");
+			doGet(request, response);
+		}
+
+	}
+
+
